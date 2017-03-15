25e5156ea1fcbf9b2947512fdcddde6a6d85d0b2
PandaForce1603 <andreas-475@hotmail.com>
2017-03-16 00:47:23 +0100

    CustomButton + changeImage

--- src/main/HTL_Odysseus.java
+++ src/main/HTL_Odysseus.java
@@ -8,31 +8,32 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import javax.swing.*;
 
-
 public class HTL_Odysseus {
 
 	public static void main(String[] args) throws IOException {
 
 		setupMainWindow();
-		
+
 	}
-	
-	public static void setupMainWindow() throws IOException{
-		/*Getting Client Version*/
-		URL clientVersionInput = new URL("http://godndr.comxa.com/versionClient"); //Change server location
-	    BufferedReader in = new BufferedReader(new InputStreamReader(clientVersionInput.openStream()));
-	    String clientVersionOutput;
-	    clientVersionOutput = in.readLine();
-	    in.close();	
-	    
-	    /*Getting Client Icon*/
+
+	public static void setupMainWindow() throws IOException {
+		/* Getting Client Version */
+		URL clientVersionInput = new URL("http://godndr.comxa.com/versionClient"); // Change
+																					// server
+																					// location
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientVersionInput.openStream()));
+		String clientVersionOutput;
+		clientVersionOutput = in.readLine();
+		in.close();
+
+		/* Getting Client Icon */
 		String appdata = System.getenv("APPDATA");
 		String iconPath = appdata + "\\HTL-Tools\\htl_icon.png";
 		File icon = new File(iconPath);
 		ImageIcon imgicon = new ImageIcon(iconPath);
-	   	
-		/*Main Window Setup*/
-		JFrame f = new JFrame("HTL - Odysseus  V" + clientVersionOutput );		
+
+		/* Main Window Setup */
+		JFrame f = new JFrame("HTL - Odysseus  V" + clientVersionOutput);
 		f.setSize(980, 620);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().setBackground(Color.DARK_GRAY);
@@ -41,6 +42,9 @@ public class HTL_Odysseus {
 		f.setResizable(false);
 		f.setIconImage(imgicon.getImage());
 		
-		JButton button = new CustomButton(f, "KV-Diagramm", 200, 100, 490, 310);
+		CustomButton button = new CustomButton(f, 200, 100, 390, 310);
+		button.changeIcon("/resources/circuit.jpg");
+		
+		
 	}
 }
