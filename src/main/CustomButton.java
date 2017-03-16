d57712942f26855173b045e30b4923e24892f6f7
PandaForce1603 <andreas-475@hotmail.com>
2017-03-16 11:34:14 +0100

    Vector Diagram Sub Program

--- src/main/CustomButton.java
+++ src/main/CustomButton.java
@@ -1,26 +1,39 @@
 package main;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
+
+import data.Application;
+
+@SuppressWarnings("unused")
 
 public class CustomButton extends JButton {
 	private static final long serialVersionUID = 1289736459726L;
 	private JFrame frame;
 	private JPanel panel;
+	private Application app;
 
-	public CustomButton(JFrame frame, int width, int height, int x, int y) {
+	public CustomButton(JFrame frame, Application app, int width, int height, int x, int y) {
+		this.app = app;
 		this.frame = frame;
 		this.setSize(width, height);
 		frame.add(this);
 		this.setLocation(x, y);
 	}
 
-	public CustomButton(JPanel panel, int width, int height, int x, int y) {
+	public CustomButton(JPanel panel, Application app, int width, int height, int x, int y) {
+		this.app = app;
 		this.panel = panel;
 		this.setSize(width, height);
 		panel.add(this);
 		this.setLocation(x, y);
+	}
+
+	public Application getApp() {
+		return app;
+	}
+
+	public void setApp(Application app) {
+		this.app = app;
 	}
 
 	public void changeIcon(String resource) {
